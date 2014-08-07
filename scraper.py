758ba09f4922c07d02408869aa6e43576eda1d18
Mosab Ahmad <mahmad@yubb-software.com>
2014-08-07 22:09:37 +0300

    Corrected the url, used requests instead, converted the script into a Flask app

--- scraper.py
+++ scraper.py
@@ -1,10 +1,60 @@
-import urllib, datetime, os
+import os
+import datetime
+import json
+import requests
 
-def fetch():
-    url = 'http://loadmeter.egyptera.org/ClockToolTip.aspx'
-    output = datetime.datetime.now().strftime('egyptera.%Y-%m-%d-%H-%M-%S.html')
-    output = os.path.join(os.path.dirname(__file__), output)
-    content = urllib.urlretrieve(url, output)
+from flask import Flask
+
+app = Flask(__name__)
+
+
+def save_content(content):
+    file_name = datetime.datetime.now().strftime('egyptera.%Y-%m-%d-%H-%M-%S.html')
+    file_name = os.path.join(os.path.dirname(__file__), file_name)
+    with open(file_name, 'w') as f:
+        f.write(content)
+
+def get_grid_status():
+    url = 'http://loadmeter.egyptera.org/MiniCurrentLoadClock3.aspx'
+    result = 'Unknown'
+
+
+
+    r = requests.get(url)
+
+    save_content(r.content)
+
+    if 'Images/c1.gif' in r.text:
+        result = 'Normal'
+
+    if 'Images/c2.gif' in r.text:
+        result = 'Warning'
+
+    if 'Images/c3.gif' in r.text:
+        result = 'Danger'
+
+    return result
+
+@app.route('/')
+def hello_world():
+    status_to_response = {
+        'Danger': 'Power grid is now in the Danger Zone!',
+        'Warning': 'Power grid is now in the Warning Zone!',
+        'Normal': 'Power grid is now in the Normal Zone!',
+    }
+    status = get_grid_status()
+
+    return status_to_response[status]
+
+
+
+@app.route('/status')
+def grid_status():
+    status = get_grid_status()
+    response = {}
+    response['status'] = status
+    return json.dumps(response)
 
 if __name__ == '__main__':
-    fetch()
+    app.debug = True
+    app.run()
